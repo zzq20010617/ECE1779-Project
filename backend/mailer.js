83e5e390e163df9efe899b935bcba0c90a5ebe01
Fireball0520 <changsun.990520@gmail.com>
2025-11-26 00:25:18 -0500

    Implement email notification feature
    - integrate email notifications when creating a new registration, updating a existing registration, or deleting a registration
    - using Mailtrap Sandbox

---
+++ backend/mailer.js
@@ -0,0 +1,12 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASS,
+    },
+});
+
+export default transporter;
